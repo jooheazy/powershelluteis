a7cad007ab3608924212b9c80478bf1533982518
Your Name <you@example.com>
2021-05-28 22:00:21 -0300

    iniciando repo com codigo topzin

---
+++ validasite/validasite.ps1
@@ -0,0 +1,25 @@
+﻿$uri = "https://google.com"
+
+try{
+        #For proxy systems
+        [System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy()
+        [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+        #Web request
+        $req = [system.Net.WebRequest]::Create($uri)
+        $res = $req.GetResponse()
+}catch {
+        #Err handling
+        $res = $_.Exception.Response
+}
+$req = $null
+
+#Getting HTTP status code
+$int = [int]$res.StatusCode
+ #Writing on the screen
+Write-Host "$int - $uri"
+
+#Disposing response if available
+if($res){
+$res.Dispose()
+}
